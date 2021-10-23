28a6d2910ce565be8a1dcb397151b87e9afa56d4
石翔宇 <xyu.shi@outlook.com>
2021-10-23 20:22:05 +0800

    update

--- labs/lab2/report/codes/gradientdescent.py
+++ labs/lab2/report/codes/gradientdescent.py
@@ -4,13 +4,14 @@ from data import *
 from uci_data import *
 from utils import *
 
-class newton:
-    def __init__(self, dataset, epochs=70000, delta=1e-7, lambda_=1e-3):
+class gradient_descent:
+    def __init__(self, dataset, lr=1e-1, epochs=70000, delta=1e-7, lambda_=1e-3):
         """
-        牛顿法初始化
+        梯度下降法初始化
 
         Args:
             dataset (tuple): 训练样本
+            lr (float), optional): 学习率. Defaults to 1e-1.
             epochs (int, optional): 最大迭代次数. Defaults to 70000.
             delta (float), optional): 早停策略的参数值. Defaults to 1e-7.
             lambda_ (float), optional): 正则项系数. Defaults to 1e-3.
@@ -19,39 +20,29 @@ class newton:
         self.dataset = dataset
         self.X = transform(dataset[0])
         self.Y = dataset[1]
+        self.lr = lr
         self.epochs = epochs
         self.delta = delta
 
-    def hessian(self, beta):
+    def gradient_descent(self):
         """
-        生成黑塞矩阵
-        
-        Args:
-            beta (array): beta
-        Returns:
-            array: 黑塞矩阵
-        """
-        exp = sigmoid(self.X @ beta)
-        H = -self.X.T @ self.X * (exp.T @ (exp - 1)) / self.X.shape[0]
-        return H
-
-    def newton(self):
-        """
-        牛顿法
+        梯度下降法
 
         Returns:
             tuple: (beta, 总迭代次数)
         """
         beta = np.random.normal(size=(self.X.shape[1], 1))
+        lr_scheduler = lr_scheduler_MultiStep(self.epochs, self.lr)
         loss_last = loss = calc_loss(self.X, beta, self.Y, self.lambda_)
         last_epoch = self.epochs
         for epoch in range(self.epochs):
-            beta = beta - np.linalg.pinv(self.hessian(beta)) @ calc_gradient(self.X, beta, self.Y, self.lambda_)
+            beta = beta - self.lr * calc_gradient(self.X, beta, self.Y, self.lambda_)
             loss = calc_loss(self.X, beta, self.Y, self.lambda_)
             if early_stop(loss, loss_last, self.delta):
                 last_epoch = epoch
                 break
             else: loss_last = loss
+            self.lr = lr_scheduler.step(epoch)
         return beta, last_epoch
 
 # 画图
@@ -67,10 +58,10 @@ def draw(train_data, test_data):
 
     plt.figure(figsize=(12, 6))
     plt.subplot(1, 2, 1)
-    beta, last_epoch = newton(train_data).newton()
+    beta, last_epoch = gradient_descent(train_data).gradient_descent()
     draw_(test_data, beta, last_epoch, 'with regularization')
     plt.subplot(1, 2, 2)
-    beta, last_epoch = newton(train_data, lambda_=None).newton()
+    beta, last_epoch = gradient_descent(train_data, lambda_=None).gradient_descent()
     draw_(test_data, beta, last_epoch, 'without regularization')
     
     plt.show()
@@ -92,10 +83,10 @@ def draw_3d(train_data, test_data):
         plt.legend()
         plt.show()
 
-    beta, last_epoch = newton(train_data).newton()
+    beta, last_epoch = gradient_descent(train_data, lr=1e-4, epochs=7000).gradient_descent()
     draw_(test_data, beta, last_epoch, 'with regularization')
     
-    beta, last_epoch = newton(train_data, lambda_=None).newton()
+    beta, last_epoch = gradient_descent(train_data, lr=1e-4, epochs=7000, lambda_=None).gradient_descent()
     draw_(test_data, beta, last_epoch, 'without regularization')
     
 
